72a2936be3ba50959ae1d8be4ccfb71b34dbbdff
l.rb1c20 <l.r.bardon@soton.ac.uk>
2021-07-21 13:30:04 +0100

    core app now working

--- runscript.py
+++ runscript.py
@@ -8,7 +8,7 @@ basepath = Path(os.path.abspath(__file__)).parents[0]
 
 # maybe conditional in here - if ldap userlist > 30 days, launch user update gui
 
-run(["python", f"{basepath}/src/views/WelcomeGui.py"])
+run(["python", f"{basepath}/src/controllers/GuiController.py"])
 
 # run(["python", f"{basepath}/src/models/collect_results.py"])
 
--- src/controllers/GuiController.py
+++ src/controllers/GuiController.py
@@ -1,34 +1,22 @@
 import sys, time, os
-from pathlib import Path
-from subprocess import run
-from PyQt5.QtWidgets import (QPushButton, QHBoxLayout, QApplication, QWidget, QProgressBar, QLabel, 
-QFrame, QVBoxLayout, QFileDialog, QVBoxLayout, QPushButton, QMainWindow, QDialog)
-from PyQt5.QtCore import Qt, QTimer, QThread, pyqtSignal, QObject, pyqtSlot
-from PyQt5.QtGui import QFont, QColor
-from PySide2 import QtCore, QtGui, QtWidgets
-from PySide2.QtCore import (QCoreApplication, QPropertyAnimation, QDate, QDateTime, QMetaObject, QObject, QPoint, QRect, QSize, QTime, QUrl, Qt, QEvent)
-from PySide2.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont, QFontDatabase, QIcon, QKeySequence, QLinearGradient, QPalette, QPainter, QPixmap, QRadialGradient)
+from PyQt5.QtWidgets import QApplication, QWidget, QProgressBar, QVBoxLayout, QFileDialog, QPushButton, QMainWindow, QDialog
+from PyQt5.QtCore import QTimer, QThread, pyqtSignal
 from PySide2.QtWidgets import *
-
 from qt_material import apply_stylesheet
 
 from src.controllers import AnalysisController
 from src.models import ParseData, GenerateResults, Save
-from ui_setup.ui_splash import Ui_SplashScreen
-
-basepath = Path(os.path.abspath(__file__)).parents[2]
-stylesheets = f"{basepath}/ui_setup"
-
+from src.views.ui_splash import Ui_SplashScreen
 
 
 class SplashScreen(QMainWindow):
-
     def __init__(self):
-
         QMainWindow.__init__(self)
+
         # set UI from ui_setup
         self.ui = Ui_SplashScreen()
         self.ui.setupUi(self)
+
         # loading progressbar timer
         self.timer = QTimer()
         self.timer.timeout.connect(self.loading)
@@ -54,51 +42,54 @@ class Start(QWidget):
         super().__init__()
         self.setWindowTitle("Health & Safety")
         self.setFixedSize(600, 500)
-        self.pbar = QProgressBar(self)
-        self.pbar.setGeometry(30, 40, 500, 75)
-        self.pbar.setMinimum(0)
-        self.pbar.setMaximum(0)
+        self.progress = QProgressBar(self)
+        self.progress.setGeometry(30, 40, 500, 75)
+        self.progress.setMinimum(0)
+        self.progress.setMaximum(0)
         self.select_button = QPushButton("Select Spreadsheet")
-        self.start_button = QPushButton("Start")
 
-        layout = QVBoxLayout()
-        self.setLayout(layout)
-        layout.addWidget(self.pbar)
-        layout.addWidget(self.select_button)
-        layout.addWidget(self.start_button)
-        self.pbar.hide()
-        self.start_button.hide()
+        self.layout = QVBoxLayout()
+        self.setLayout(self.layout)
+        self.layout.addWidget(self.progress)
+        self.layout.addWidget(self.select_button)
+        self.progress.hide()
 
+        # bind filebrowser dialog window to button click
         self.select_button.clicked.connect(self.launch_browser)
 
 
     def launch_browser(self):
         FileBrowser()
-     
-        apply_stylesheet(app, theme='dark_blue.xml') 
-        time.sleep(1)
-        self.start_tasks()
-        # self.close()
+        self.select_button.hide()
+
+        # bind controls to events
+        self.start_button = QPushButton("Start")
+        self.layout.addWidget(self.start_button)
+        self.start_button.clicked.connect(self.begin_analysis)   
 
 
-    def start_tasks(self):
-        self.select_button.clear()
+    def begin_analysis(self):
+        self.start_button.hide()
+        self.progress.show()
 
-        self.start_button.setCheckable(True)
-        self.start_button.toggle()
-        self.start_button.show()
+        # set up thread for analysis
+        self.analysis = Analysis()
+        self.simulThread = QThread()
+
+        self.analysis.moveToThread(self.simulThread)
+        self.analysis.started.connect(self.analysis.run)
+        self.analysis.finished.connect(self.analysis_complete)
+
+        self.analysis.completed.connect(self.analysis.stop)
+        self.analysis.start()
 
 
-        # if self.start_button.clicked.connect(self.run_tasks):
-        if self.start_button.isChecked():
-            self.start_button.hide()
-            self.pbar.show()
-            self.run_tasks()
-
-
+    def analysis_complete(self):
+        print('Im done :D')
+        self.progress.hide()
+        
 
     def run_tasks(self):
-
         time.sleep(3)
         breakpoint()
         raw_csv = ParseData.excel_to_csv(FILE)
@@ -113,20 +104,47 @@ class Start(QWidget):
         faculty_results = GenerateResults.results_by_faculty(faculties, not_completed)
         Save.save_faculties_dataframes(faculty_results)
         GenerateResults.results_by_department(faculty_results)
-            
 
 
+class Analysis(QThread):
+    """
+    Runs a counter thread for processing calculations
+    """
+    completed = pyqtSignal()
+
+    def __init__(self, parent=None):
+        super(Analysis, self).__init__(parent)
+        self.running = True
+
+
+    def run(self):
+        while self.running:
+            print("doing stuff")
+            time.sleep(1)
+            print("doing more stuff")
+            time.sleep(2)
+            print("done")
+            break
+
+        self.completed.emit()
+
+
+    def stop():
+        print('finished stuff')
+        self.running = False
+
+        
 
 class FileBrowser(QDialog):
 
     def __init__(self):
         super().__init__()
-        apply_stylesheet(app, theme='light_cyan_500.xml',  invert_secondary=True) 
+        # apply_stylesheet(app, theme='light_cyan_500.xml',  invert_secondary=True) 
         self.setFixedSize(600, 500)
         self.open_filename_dialog()     
         self.show()
 
-    @pyqtSlot(str)
+    # @pyqtSlot(str)
     def open_filename_dialog(self):
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
@@ -135,8 +153,10 @@ class FileBrowser(QDialog):
         if file_name:
             global FILE
             FILE = file_name
-            return
-            # self.close()
+            # apply_stylesheet(app, theme='dark_blue.xml') 
+            # return
+            self.close()
+            # time.sleep(3)
     
 
 
@@ -149,10 +169,10 @@ if __name__ == '__main__':
         # extra = open(f"{stylesheets}/launch.css").read()
         app = QApplication(sys.argv)
         apply_stylesheet(app, theme='dark_blue.xml') 
-        stylesheet = app.styleSheet()
+        # stylesheet = app.styleSheet()
 
-        with open(f"{stylesheets}/launch.css") as file:
-            app.setStyleSheet(stylesheet + file.read())
+        # with open(f"{stylesheets}/launch.css") as file:
+        #     app.setStyleSheet(stylesheet + file.read())
 
         window = SplashScreen()
         window.show()
@@ -160,5 +180,3 @@ if __name__ == '__main__':
 
     except SystemExit:
         print('Closing Window...')
-
-
