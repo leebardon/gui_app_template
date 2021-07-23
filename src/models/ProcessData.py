64761ebe5698dbb6c02d38facccff925427b7b05
l.rb1c20 <l.r.bardon@soton.ac.uk>
2021-07-23 13:21:21 +0100

    full MVP with docstrings

--- src/models/ProcessData.py
+++ src/models/ProcessData.py
@@ -26,23 +26,37 @@ class StringConverter(dict):
 
 
 def excel_to_csv(T_RECORDS_PATH, NOT_COMPLETED_PATH):
-    TR_csv = f"{_v.BASEPATH}/data/interim_data/interim-training-records-{_v.TODAY}.csv"
-    NC_csv = f"{_v.BASEPATH}/data/interim_data/interim-incomplete-courses-{_v.TODAY}.csv"
+    """ Converts excels spreadsheet to csv for improved processing speed
+        Currently proceeds only if conversion has not been done for the current day...
+    Args:
+        T_RECORDS_PATH (path): To student_training_records.xlsx
+        NOT_COMPLETED_PATH (path): To not_completed.xlsx
+    Returns:
+        [type]: [description]
+    """
+    TR_csv_path = f"{_v.BASEPATH}/data/interim_data/interim-training-records-{_v.TODAY}.csv"
+    NC_csv_path = f"{_v.BASEPATH}/data/interim_data/interim-incomplete-courses-{_v.TODAY}.csv"
 
-    for csv in [TR_csv, NC_csv]:
+    for p in [TR_csv_path, NC_csv_path]:
         for data in [T_RECORDS_PATH, NOT_COMPLETED_PATH]:
-            if not os.path.exists(csv):
-                Xlsx2csv(data, outputencoding="utf-8").convert(csv)
+            if not os.path.exists(p):
+                Xlsx2csv(data, outputencoding="utf-8").convert(p)
 
-    return TR_csv, NC_csv
+    return TR_csv_path, NC_csv_path
 
 
 
 def csv_to_df(TR_csv, NC_csv):
+    """ Converts csv to pandas dataframe for processing
+    Args:
+        TR_csv (csv file): Training records csv
+        NC_csv (csv file): Non complete csv
+    """
     return map(lambda x: pd.read_csv(x, converters=StringConverter()), [TR_csv, NC_csv])
 
 
 def remove_dash(*dfs):
+    """ Converts dash to Nonetype to enable further processing """
     out = []
     for df in dfs:
         df.replace({"-": None}, inplace=True) 
@@ -65,6 +79,11 @@ def remove_dash(*dfs):
 
 
 def df_to_csv(tr_interim, nc_interim):
+    """ Saves processed dataframes as interim csv files 
+    Args:
+        tr_interim (pandas dataframe): Training records df
+        nc_interim (pandas dataframe): Training records df
+    """
     tr_interim.to_csv(
         f"{_v.BASEPATH}/data/processed_data/all/training-records-{_v.TODAY}.csv"
     )
@@ -72,7 +91,4 @@ def df_to_csv(tr_interim, nc_interim):
         f"{_v.BASEPATH}/data/processed_data/all/incomplete-courses-{_v.TODAY}.csv"
     )
 
-# def return_dataframe():
-#     return pd.read_csv(
-#         f"{_v.BASEPATH}/data/processed_data/all/incomplete-courses-{_v.TODAY}.csv"
-#     )
+
